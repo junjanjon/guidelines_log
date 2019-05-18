0a7671ee99b934af6f73e706cf27d4dcbd6f2610
junki.hiroi <junki.hiroi@drecom.co.jp>
2019-05-19 03:32:11 +0900

    fix script

--- diff_commit.sh
+++ diff_commit.sh
@@ -1,14 +1,23 @@
+#!/usr/bin/env bash -xe
+
+set -e
 
 TODAY=`date +%Y%m%d`
 WATCH_FILE_JP=guidelines_jp.html
 WATCH_FILE_EN=guidelines_en.html
 
-# git reset --hard HEAD
-# git clean -fdx
+git reset --hard HEAD
+git clean -fdx
 curl -o ${WATCH_FILE_JP} 'https://developer.apple.com/jp/app-store/review/guidelines/index.html'
-git add ${WATCH_FILE_JP}
 curl -o ${WATCH_FILE_EN} 'https://developer.apple.com/app-store/review/guidelines/index.html'
-git add ${WATCH_FILE_EN}
+
+git add -N .
+
+set +e
 git diff --exit-code --quiet
-if [[ $? -eq 1 ]]; then git commit -m "diff commit $(TODAY)"; fi
+if [[ $? -eq 0 ]]; then exit; fi
+
+git add ${WATCH_FILE_JP}
+git add ${WATCH_FILE_EN}
+git commit -m "diff commit ${TODAY}"
 
